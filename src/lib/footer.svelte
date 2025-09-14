e98c56d93add0ee5cfc0aa44b80d7519734a1370
Harrison Phillingham <harrison@phillingham.com>
2025-09-14 14:13:28 +0100

    v0.1.0 - Added version number and temporary description

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,9 +1,14 @@
 
+<script>
+
+    import {version_number} from "$lib/version_number.js";
+</script>
+
 <div class="flex flex-col justify-center items-center content-center align-middle left-0 bottom-1 right-0 p-4 m-auto align-content-center select-none">
     <div class="flex">
         <a href="https://github.com/Harry55494/portfolio-v3" target="_blank" class="text-sm flex items-center text-[12px]">
             <img src="https://cdn.simpleicons.org/git" alt="git_logo" class="h-5 w-auto mr-2">
-            Source
+            Source (v{version_number})
         </a>
     </div>
     <p class="mt-2 text-[12px]">© {new Date().getFullYear()} Harrison Phillingham</p>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,8 +2,10 @@
 </script>
 
 <div class="m-auto">
-    <h1 class="text-3xl font-bold mt-4 mb-2">About</h1>
+    <h1 class="text-3xl font-bold mt-4 mb-4">About</h1>
 
-    <p></p>
+    <p>Hey! My name's Harrison and I work in Digital Forensics for Thames Valley Police.</p>
+
+    <p class="mt-2">This website is under construction, but you can find out more about me on <a class="underline hover:text-cyan-500 transition-colors duration-200" href="https://www.linkedin.com/in/harrison-phillingham/">LinkedIn</a>, or see some of my projects on the <a class="underline hover:text-cyan-500 transition-colors duration-200" href="/projects">Projects</a> page.</p>
 
 </div>
